92605eec3282bb08e5d0d35e09ab7e8c50690cf3
arturu <arturu@arturu.it>
2024-12-27 16:00:27 +0100

    paragraph node gallery funzionante

---
+++ sql/node_notizia_paragraph_gallery_item.sql
@@ -0,0 +1,20 @@
+SELECT
+  CONCAT('article_',galleria.entity_id,'--gallery-item_',galleria.field_galleria_fid) AS 'migration_id'
+  , CASE
+      WHEN galleria.language IS NULL OR galleria.language = 'und' THEN 'it'
+      ELSE galleria.language
+    END AS 'langcode'
+  , galleria.field_galleria_fid AS 'target_id'
+  , CASE
+      WHEN galleria.field_galleria_alt IS NULL OR galleria.field_galleria_alt = '' THEN galleria.field_galleria_title
+      ELSE galleria.field_galleria_alt
+  END AS 'alt'
+  , galleria.field_galleria_title AS 'title'
+  , galleria.field_galleria_width AS 'width'
+  , galleria.field_galleria_height AS 'height'
+  , node.created AS 'created'
+FROM field_data_field_galleria galleria
+  LEFT JOIN node node ON galleria.entity_id = node.nid
+WHERE galleria.bundle IN ('article', 'blog')
+ORDER BY galleria.entity_id
+;
